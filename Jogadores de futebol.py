d2d3d82a004a6993fe5f89bf5fe9e805a97433a8
Sharron <112340168+SharronLima@users.noreply.github.com>
2023-05-11 11:17:43 -0300

    Add files via upload

---
+++ Jogadores de futebol.py
@@ -0,0 +1,21 @@
+# Atacantes de futbeol RealM.
+
+# Declarando a variáveis de jogadores
+nome = input("Digite um nome: ")
+jogador = nome.lower()
+
+# Condições de nomes
+if 'benzema' in jogador:
+    print("Este jogadro é do real madird.")
+
+if 'vinicius' in jogador:
+    print("Este jogadro é do real madird.")
+
+if 'asensio' in jogador:
+    print("Este jogadro é do real madird.")
+
+if 'rodrygo' in jogador:
+    print("Este jogadro é do real madird.")
+
+else:
+    print("Este jogador não é do real madrid")
